ba0cc463ca3af6bdb89f5cb9e2d55b2f2db9d159
EMMANUEL OBIEZUO <emmanuelobiezuo@gmail.com>
2023-04-15 21:15:13 +0800

    arranged the states

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -5,8 +5,48 @@ import Data from '../Assets/data.json'
 import deleteicon from '../Assets/avatars/icon-delete.svg'
 import editicon from '../Assets/avatars/icon-edit.svg'
 
-const Comment = ({ data, reply, switchReply,handleReply,text,setText }) => {
+const Comment = ({data, comments, setComments ,text,setText }) => {
   const { createdAt, content, score, user:{image:{png,webp},username}, replies, id } = data
+  const[edit,setEdit] = useState(false)
+  const[reply,setReply] = useState(false)
+   const switchReply = ()=>setReply(!reply)
+   const handleReply = (id)=>{
+        const updatedComment =[...comments]
+        const commentIndex = comments.findIndex(i=>i===id)
+         const newreply = {id:new Date().getTime().toString(), content:text}
+         updatedComment[commentIndex].replies.push(newreply);
+         setComments(updatedComment)
+         setReply(false)
+         setText('')
+        console.log(comments)
+    }
+     const OnEdit =(id)=>{
+        setEdit(true)
+        const EditedComment = comments.find(i=>i.id===id) 
+        setText(EditedComment.content)
+        console.log(text)
+    }
+     const handleEdit= (id)=>{
+        if(text && edit){
+            setComments(
+                comments.map(item=>{
+                if(item.id===id){
+                    return{...item,content:text}
+                }
+                return item
+       })    
+            )
+        }
+        setEdit(false)
+       console.log(comments)
+    }  
+     const handleDelete = (id)=>{
+        return replies.filter(item => item.id !== id) 
+    }
+   
+
+
+ 
 
   return (
     <>
@@ -51,7 +91,7 @@ const Comment = ({ data, reply, switchReply,handleReply,text,setText }) => {
                         <p>{username}</p>
                         <small>{createdAt}</small>
                       </div>
-                      {Data.currentUser.username === username ? <div><button><img src={deleteicon} alt='delete-icon' />Delete</button><button><img src={editicon} alt='edit-icon' />edit</button></div>:<button><img src={replyicon} alt='reply-icon' />reply</button> }
+                      {Data.currentUser.username === username ? <div><button onClick={()=>handleDelete(id)}><img src={deleteicon} alt='delete-icon' />Delete</button><button onClick={()=>OnEdit(id)}><img src={editicon} alt='edit-icon' />edit</button></div>:<button onClick={switchReply}><img src={replyicon} alt='reply-icon' />reply</button> }
                     </div>
                     <p>{`@${replyingTo}  ${content}`}</p>
                   </div>
--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -8,49 +8,13 @@ import Comment from './Comment'
 
 const Comments = () => {
     const[comments,setComments] = useState(Data.comments)
-    const[text,setText] = useState('')
-    const[reply,setReply] = useState(false)
-    const[edit,setEdit] = useState(false)
-    const switchReply = ()=>setReply(true)
-    
-    const handleReply = (id)=>{
-        const updatedComment =[...comments]
-        const commentIndex = comments.findIndex(i=>i===id)
-         const newreply = {id:new Date().getTime().toString(), content:text}
-         updatedComment[commentIndex].replies.push(newreply);
-         setComments(updatedComment)
-         setReply(false)
-         setText('')
-        console.log(comments)
-    }
-    const handleDelete = (id)=>{
-        comments.filter(item => item.id !== id)
-    }
-    const OnEdit =(id)=>{
-        setEdit(true)
-        const EditedComment = comments.find(i=>i.id===id) 
-        setText(EditedComment.content)
-    }
-    const handleEdit= (id)=>{
-        if(text && edit){
-            setComments(
-                comments.map(item=>{
-                if(item.id===id){
-                    return{...item,content:text}
-                }
-                return item
-       })    
-            )
-        }
-        setEdit(false)
-       console.log(comments)
-    }  
+    const[text,setText] = useState('')   
   return (
     <section>
         {
             comments.map((data,i)=>{
                 return(
-                    <Comment key={data.id} data={data} reply={reply} switchReply={switchReply} handleReply={handleReply} text={text} setText={setText}/>
+                    <Comment key={data.id} comments={comments} setComments={setComments} data={data} text={text} setText={setText} />
                 )
             })
         } 
